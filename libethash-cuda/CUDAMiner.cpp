bbd8c4199f4ea3821ab13a0f729bb2831eff42fa
Paweł Bylica <chfast@gmail.com>
2017-12-28 20:01:06 +0100

    CUDAMiner: Use composition for ethash_cuda_miner

--- libethash-cuda/CUDAMiner.cpp
+++ libethash-cuda/CUDAMiner.cpp
@@ -14,12 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
-/** @file CUDAMiner.cpp
-* @author Gav Wood <i@gavwood.com>
-* @date 2014
-*
-* Determines the PoW algorithm.
-*/
 
 #include "CUDAMiner.h"
 
@@ -97,7 +91,6 @@ CUDAMiner::CUDAMiner(FarmFace& _farm, unsigned _index) :
 CUDAMiner::~CUDAMiner()
 {
 	pause();
-	delete m_miner;
 	delete m_hook;
 }
 
@@ -124,14 +117,11 @@ bool CUDAMiner::init(const h256& seed)
 		cnote << "Initialising miner...";
 		m_minerSeed = seed;
 
-		if(!m_miner)
-			m_miner = new ethash_cuda_miner;
-
 		EthashAux::LightType light;
 		light = EthashAux::light(seed);
 		bytesConstRef lightData = light->data();
 
-		m_miner->init(light->light, lightData.data(), lightData.size(), 
+		m_miner.init(light->light, lightData.data(), lightData.size(),
 			device, (s_dagLoadMode == DAG_LOAD_MODE_SINGLE), s_dagInHostMemory);
 		s_dagLoadIndex++;
     
@@ -149,8 +139,6 @@ bool CUDAMiner::init(const h256& seed)
 	}
 	catch (std::runtime_error const& _e)
 	{
-		delete m_miner;
-		m_miner = nullptr;
 		cwarn << "Error CUDA mining: " << _e.what();
 		return false;
 	}
@@ -167,7 +155,7 @@ void CUDAMiner::workLoop()
 		{
 			const WorkPackage w = work();
 			
-			if(!m_miner || current.header != w.header || current.seed != w.seed)
+			if (current.header != w.header || current.seed != w.seed)
 			{
 				if(!w || w.header == h256())
 				{
@@ -177,7 +165,7 @@ void CUDAMiner::workLoop()
 				}
 				
 				//cnote << "set work; seed: " << "#" + w.seed.hex().substr(0, 8) + ", target: " << "#" + w.boundary.hex().substr(0, 12);
-				if (!m_miner || current.seed != w.seed)
+				if (current.seed != w.seed)
 				{
 					if(!init(w.seed))
 						break;
@@ -188,7 +176,7 @@ void CUDAMiner::workLoop()
 			uint64_t startN = current.startNonce;
 			if (current.exSizeBits >= 0) 
 				startN = current.startNonce | ((uint64_t)index << (64 - 4 - current.exSizeBits)); // this can support up to 16 devices
-			m_miner->search(current.header.data(), upper64OfBoundary, *m_hook, (current.exSizeBits >= 0), startN);
+			m_miner.search(current.header.data(), upper64OfBoundary, *m_hook, (current.exSizeBits >= 0), startN);
 			
 			// Check if we should stop.
 			if (shouldStop())
@@ -199,8 +187,6 @@ void CUDAMiner::workLoop()
 	}
 	catch (std::runtime_error const& _e)
 	{
-		delete m_miner;
-		m_miner = nullptr;
 		cwarn << "Error CUDA mining: " << _e.what();
 	}
 }
@@ -208,11 +194,6 @@ void CUDAMiner::workLoop()
 void CUDAMiner::pause()
 {
 	m_hook->abort();
-}
-
-std::string CUDAMiner::platformInfo()
-{
-	return ethash_cuda_miner::platform_info(s_deviceId);
 }
 
 unsigned CUDAMiner::getNumDevices()
@@ -227,11 +208,7 @@ void CUDAMiner::listDevices()
 
 HwMonitor CUDAMiner::hwmon()
 {
-	HwMonitor hw;
-	if (m_miner) {
-		hw = m_miner->hwmon();
-	}
-	return hw;
+	return m_miner.hwmon();
 }
 
 bool CUDAMiner::configureGPU(
